95c7012d0155b5499ea4ed38e37848d49e30de48
Minhhieu3012 <hieupm9898@ut.edu.vn>
2025-01-13 09:42:18 +0700

    create urls & update

--- Pet_Health_Care_System/Pet_Cus_Info_Mng/urls.py
+++ Pet_Health_Care_System/Pet_Cus_Info_Mng/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
-
+from .views import(
+    PetListView,PetDetailView,PetCreateView,PetUpdateView,PetDeleteView
+)
 urlpatterns = [
     path('Pet_Cus_Info_Mng/pets/', views.pet_list, name='pet_list'), #url cho pet
     path('Pet_Cus_Info_Mng/customers/', views.customer_list, name='customer_list'), #url cho customer
+    
+    path('pets/', PetListView.as_view(), name='pet_list'),  # Danh sách Pet
+    path('pets/<int:pk>/', PetDetailView.as_view(), name='pet_detail'),  # Chi tiết Pet
+    path('pets/add/', PetCreateView.as_view(), name='pet_add'),  # Thêm Pet
+    path('pets/<int:pk>/edit/', PetUpdateView.as_view(), name='pet_edit'),  # Sửa Pet
+    path('pets/<int:pk>/delete/', PetDeleteView.as_view(), name='pet_delete'),  # Xóa Pet
 ]
