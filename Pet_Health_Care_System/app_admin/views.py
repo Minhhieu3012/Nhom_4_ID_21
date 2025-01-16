bcaf92b1423a6beaa38541d74115efd6387eca65
Minhhieu3012 <hieupm9898@ut.edu.vn>
2025-01-16 23:55:47 +0700

    update file view

--- Pet_Health_Care_System/app_admin/views.py
+++ Pet_Health_Care_System/app_admin/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def app_admin(request):
+    return render(request,'app_admin/app_admin.html')
