1cacce31cfd30fde0b32c013ea2e05761896701b
“Dapaull” <ngbao840@gmail.com>
2025-01-17 11:21:46 +0700

    add file urls.py cho app

--- Pet_Health_Care_System/app_admin/admin.py
+++ Pet_Health_Care_System/app_admin/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Pet, Owner, Room, Admission, Staff, Invoice
 # Register your models here.
+admin.site.register(Pet, Owner, Room, Admission, Staff, Invoice)
