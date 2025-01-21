f4c62c0036ca0aa75a141e59da4a7a3b8238ca5b
“Dapaull” <ngbao840@gmail.com>
2025-01-21 10:17:09 +0700

    startapp mng_of_barnRoom_and_hosService

---
+++ Pet_Health_Care_System/mng_of_barnRoom_and_hosService/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MngOfBarnroomAndHosserviceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mng_of_barnRoom_and_hosService'
