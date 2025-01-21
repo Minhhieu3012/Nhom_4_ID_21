6ec3cba049bd00bb15ccc645a210911e7042d5d7
“Dapaull” <ngbao840@gmail.com>
2025-01-21 10:54:13 +0700

    update apps.py of MngOfBarnRoomHosService

--- Pet_Health_Care_System/MngOfBarnRoomHosService/apps.py
+++ Pet_Health_Care_System/MngOfBarnRoomHosService/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MngOfBarnroomAndHosserviceConfig(AppConfig):
+class MngOfBarnRRoomAndHosServiceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mng_of_barnRoom_and_hosService'
+    name = 'MngOfBarnRoomHosService'
