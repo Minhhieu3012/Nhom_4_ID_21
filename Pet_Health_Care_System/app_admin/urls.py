84aa57c7851fdcbd30439d8d2c9083d09742f2dd
“Dapaull” <ngbao840@gmail.com>
2025-01-26 16:19:06 +0700

    update html rooms, room-edit

--- Pet_Health_Care_System/app_admin/urls.py
+++ Pet_Health_Care_System/app_admin/urls.py
@@ -2,11 +2,14 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('app_admin/', views.Pet, name='Pet'),
-    path('app_admin/', views.Owner, name='Owner'),
-    path('app_admin/', views.Room, name='Room'),
-    path('app_admin/', views.Admission, name='Admission'),
-    path('app_admin/', views.Staff, name='Staff'),
-    path('app_admin/', views.Invoice, name='Invoice'),
+    path('app_admin/', views.pet, name='Pet'),
+    path('app_admin/', views.owner, name='Owner'),
+    path('app_admin/', views.room, name='Room'),
+    path('app_admin/', views.admission, name='Admission'),
+    path('app_admin/', views.staff, name='Staff'),
+    path('app_admin/', views.invoice, name='Invoice'),
+
     path('', views.app_admin, name='app_admin'),
-]
+    path('rooms', views.rooms, name='rooms'),
+    path('room-edit', views.room_edit, name='room-edit'),
+] 
--- Pet_Health_Care_System/app_admin/views.py
+++ Pet_Health_Care_System/app_admin/views.py
@@ -1,17 +1,35 @@
+from django.template import loader
 from django.shortcuts import render
 from django.http import HttpResponse
+from app_admin.models import Room
 
-def Pet(request):
+def pet(request):
     return HttpResponse("Hello world!")
-def Owner(request):
+def owner(request):
     return HttpResponse("Hello world!")
-def Room(request):
+def room(request):
     return HttpResponse("Hello world!")
-def Admission(request):
+def admission(request):
     return HttpResponse("Hello world!")
-def Staff(request):
+def staff(request):
     return HttpResponse("Hello world!")
-def Invoice(request):
+def invoice(request):
     return HttpResponse("Hello world!")
-def app_admin (request):
-    return render (request, 'app_admin/app_admin.html')
+def app_admin(request):
+    return render (request, 'home.html')
+
+def rooms(request):
+    rooms = Room.objects.all()
+    template = loader.get_template('app_admin/rooms.html')
+    context = {
+        'room' : rooms, 
+    }
+    return HttpResponse(template.render(context, request))
+
+def room_edit(request):
+    room = Room.objects.get(id = 1)
+    template = loader.get_template('app_admin/room-edit.html')
+    context = {
+        'room' : room, 
+    }
+    return HttpResponse(template.render(context, request))
