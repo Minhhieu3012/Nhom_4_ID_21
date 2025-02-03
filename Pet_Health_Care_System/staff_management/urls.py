ae2c1055b56523935d3d807b21b286c306cce9b2
tien <dotienatt@gmail.com>
2025-02-03 22:46:49 +0700

    taolai

--- Pet_Health_Care_System/staff_management/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('staff/', views.staff_list, name='staff_list'),
-    path('staff/<int:staff_id>/schedule/', views.staff_schedule, name='staff_schedule'),
-]
