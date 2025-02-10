fe5721940f12aa88259e057326819d7e37b30e5c
tien <dotienatt@gmail.com>
2025-02-10 22:52:55 +0700

    sua view,urls

--- Pet_Health_Care_System/staff_management/urls.py
+++ Pet_Health_Care_System/staff_management/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from .views import StaffListView, StaffDetailView, StaffCreateView, StaffUpdateView, StaffDeleteView
+from . import views
 
 urlpatterns = [
     path('staffs/', StaffListView.as_view(), name='staff_list'),
     path('staffs/<int:pk>/', StaffDetailView.as_view(), name='staff_detail'),
     path('staffs/add/', StaffCreateView.as_view(), name='staff_add'),
     path('staffs/<int:pk>/edit/', StaffUpdateView.as_view(), name='staff_edit'),
+   
     path('staffs/<int:pk>/delete/', StaffDeleteView.as_view(), name='staff_delete'),
+    path('staff/', views.staff_list, name='staff_list'),
+    path('staff/<int:id>/', views.staff_detail, name='staff_detail'),
+    path('schedule/', views.schedule_list, name='schedule_list'),
+
 ]
--- Pet_Health_Care_System/staff_management/views.py
+++ Pet_Health_Care_System/staff_management/views.py
@@ -4,6 +4,48 @@ from django.urls import reverse_lazy
 from .models import Staff, WorkingSchedule
 from .forms import StaffForm, WorkingScheduleForm
 
+
+from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Staff, Appointment, WorkingSchedule, Role, AppointmentHistory, Notification
+
+# View cho Staff
+class StaffDetailView(DetailView):
+    model = Staff
+    template_name = 'staff_detail.html'
+    context_object_name = 'staff'
+
+# View cho Appointment
+class AppointmentDetailView(DetailView):
+    model = Appointment
+    template_name = 'appointment_detail.html'
+    context_object_name = 'appointment'
+
+# View cho WorkingSchedule
+class WorkingScheduleDetailView(DetailView):
+    model = WorkingSchedule
+    template_name = 'working_schedule_detail.html'
+    context_object_name = 'schedule'
+
+# View cho Role
+class RoleDetailView(DetailView):
+    model = Role
+    template_name = 'role_detail.html'
+    context_object_name = 'role'
+
+# View cho AppointmentHistory
+class AppointmentHistoryDetailView(DetailView):
+    model = AppointmentHistory
+    template_name = 'appointment_history_detail.html'
+    context_object_name = 'appointment_history'
+
+# View cho Notification
+class NotificationDetailView(DetailView):
+    model = Notification
+    template_name = 'notification_detail.html'
+    context_object_name = 'notification'
+
+
 # Danh sách nhân viên
 class StaffListView(ListView):
     model = Staff
@@ -34,3 +76,13 @@ class StaffDeleteView(DeleteView):
     model = Staff
     template_name = 'staff_confirm_delete.html'
     success_url = reverse_lazy('staff_list')
+
+
+def staff_list(request):
+    return render(request, 'staff_management/staff_list.html')
+
+def staff_detail(request):
+    return render(request, 'staff_management/staff_detail.html')
+
+def schedule_list(request):
+    return render(request, 'staff_management/schedule_list.html')
