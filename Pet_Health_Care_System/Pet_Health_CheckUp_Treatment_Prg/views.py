ca440af2a8c94dafecf72b6a544cc22f66ad6c82
Minhhieu3012 <hieupm9898@ut.edu.vn>
2025-02-12 06:16:08 +0700

    update template 12/2/25

--- Pet_Health_Care_System/Pet_Health_CheckUp_Treatment_Prg/views.py
+++ Pet_Health_Care_System/Pet_Health_CheckUp_Treatment_Prg/views.py
@@ -2,7 +2,7 @@
 from django.contrib import messages
 from django.http import HttpResponseBadRequest, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from .models import MedicalRecord, TreatmentProgress, Medication, Pet
 from .forms import MedicalRecordForm, TreatmentProgressForm, MedicationForm, PetForm
@@ -107,7 +107,7 @@ class MedicalRecordUpdateView(UpdateView):
         form.save()
         messages.success(self.request, "Bệnh án đã được cập nhật thành công!")
         return HttpResponseRedirect(self.get_success_url())
-
+    
 
 class MedicalRecordDeleteView(DeleteView):
     model = MedicalRecord
@@ -116,8 +116,12 @@ class MedicalRecordDeleteView(DeleteView):
 
     def get_success_url(self):
         # Sử dụng self.object.pet.id để lấy pet_id của bệnh án vừa xóa
-        return reverse_lazy('medical_record_list', kwargs={'pet_id': self.object.pet.id})
+        return reverse('medical_record_list', kwargs={'pet_id': self.object.pet.id})
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["medical_record"] = self.object  # Đảm bảo truyền đúng object
+        return context
 
 # ----------------------------------------------
 # Views cho Treatment Progress (Tiến trình điều trị)
@@ -138,40 +142,47 @@ class TreatmentProgressCreateView(CreateView):
     model = TreatmentProgress
     form_class = TreatmentProgressForm
     template_name = 'Pet_Health_CheckUp_Treatment_Prg/treatment_progress_form.html'
-    success_url = reverse_lazy('treatment_progress_list')
+    
+    def form_invalid(self, form):
+        print("Form không hợp lệ:", form.errors)  # Debug lỗi trong console
+        return self.render_to_response(self.get_context_data(form=form))
 
-    def form_valid(self, form):
-        form.save()
-        messages.success(self.request, "Tiến trình điều trị đã được tạo thành công!")
-        return HttpResponseRedirect(self.get_success_url())
+    def get_success_url(self):
+        # self.object.pet chắc chắn đã được gán từ form_valid()
+        return reverse_lazy('treatment_progress_list', kwargs={'pet_id': self.object.pet.id})
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Truyền thêm danh sách bệnh án (nếu cần) để lựa chọn trong form
-        context['medical_records'] = MedicalRecord.objects.all()
+        context['pet_id'] = self.kwargs.get('pet_id')  # Truyền pet_id xuống template
         return context
+
 
 
 class TreatmentProgressUpdateView(UpdateView):
     model = TreatmentProgress
     form_class = TreatmentProgressForm
     template_name = 'Pet_Health_CheckUp_Treatment_Prg/treatment_progress_form.html'
-    success_url = reverse_lazy('treatment_progress_list')
+    
+    def form_invalid(self, form):
+        print("Form không hợp lệ:", form.errors)
+        return self.render_to_response(self.get_context_data(form=form))
 
-    def form_valid(self, form):
-        form.save()
-        messages.success(self.request, "Tiến trình điều trị đã được cập nhật thành công!")
-        return HttpResponseRedirect(self.get_success_url())
-
+    def get_success_url(self):
+        # self.object.pet phải tồn tại và có id
+        return reverse_lazy('treatment_progress_list', kwargs={'pet_id': self.object.pet.id})
 
 class TreatmentProgressDeleteView(DeleteView):
     model = TreatmentProgress
-    template_name = 'Pet_Health_CheckUp_Treatment_Prg/treatment_progress_confirm_delete.html'
-    success_url = reverse_lazy('treatment_progress_list')
+    template_name = "Pet_Health_CheckUp_Treatment_Prg/treatment_progress_delete.html"
 
-    def delete(self, request, *args, **kwargs):
-        messages.success(request, "Tiến trình điều trị đã được xóa thành công!")
-        return super().delete(request, *args, **kwargs)
+    def get_success_url(self):
+        return reverse("treatment_progress_list", kwargs={"pet_id": self.object.pet.id})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["treatment_progress"] = self.object  # Đảm bảo truyền đúng object
+        return context
+
 
 
 # ----------------------------------------------
