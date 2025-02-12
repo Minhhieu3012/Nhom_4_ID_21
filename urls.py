d57ec03a0fd964ae803441e46c399af62e89a54c
Gia Han <hanbuii7705@gmail.com>
2025-02-12 11:22:05 +0700

    Add urls

---
+++ urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('users/', views.user_list, name='user_list'),
+    path('users/create/', views.user_create, name='user_create'),
+    path('users/delete/<int:user_id>/', views.user_delete, name='user_delete'),
+]
