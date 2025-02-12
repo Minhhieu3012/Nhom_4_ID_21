0bfb7f12dde3c7325a3c5aa1cb3d7fcd6c3d215c
Gia Han <hanbuii7705@gmail.com>
2025-02-12 13:12:37 +0700

    Update Urls.py

--- Pet_Health_Care_System/app_admin/urls.py
+++ Pet_Health_Care_System/app_admin/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
-from .import views
+from . import views
+
 urlpatterns = [
     path('', views.app_admin, name='app_admin'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
